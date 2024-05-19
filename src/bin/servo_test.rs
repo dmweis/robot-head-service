14b0dc637e27c0503e387084235bb8f2032c676c
David Weis <dweis7@gmail.com>
2024-05-19 02:18:17 +0000

    Move lib to library

--- src/bin/servo_test.rs
+++ src/bin/servo_test.rs
@@ -1,12 +1,12 @@
 use std::{path::PathBuf, time::Duration};
 
-use anyhow::Result;
 use clap::Parser;
-use lss_driver::{CommandModifier, LSSDriver, MotorStatus};
-use robot_head_service::{turn_off_display, turn_on_display};
+use lss_driver::CommandModifier;
+use robot_head_service::{
+    setup_tracing, turn_off_display, turn_on_display, HeadController, NECK_MOTOR_A_NORMAL_POSITION,
+};
 use tokio::time::sleep;
 use tracing::info;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Parser, Debug)]
 #[command(version, author)]
@@ -18,97 +18,6 @@ struct Args {
     /// Serial port
     #[arg(long, default_value = "/dev/ttyUSB0")]
     port: String,
-}
-
-const BASE_MOTOR_ID: u8 = 1;
-const NECK_MOTOR_A_ID: u8 = 2;
-
-const ALL_MOTOR_IDS: [u8; 2] = [BASE_MOTOR_ID, NECK_MOTOR_A_ID];
-
-const NECK_MOTOR_A_NORMAL_POSITION: f32 = -10.0;
-
-struct HeadController {
-    driver: LSSDriver,
-}
-
-impl HeadController {
-    fn new(port: &str) -> Result<Self> {
-        let driver = lss_driver::LSSDriver::new(port)?;
-        Ok(Self { driver })
-    }
-
-    async fn configure(&mut self) -> Result<()> {
-        for id in ALL_MOTOR_IDS {
-            self.driver.set_origin_offset(id, 0.0).await?;
-            self.driver
-                .configure_color(id, lss_driver::LedColor::Off)
-                .await?;
-            self.driver
-                .set_color(id, lss_driver::LedColor::Cyan)
-                .await?;
-
-            self.driver.set_motion_profile(id, true).await?;
-
-            let position = self.driver.query_position(id).await?;
-            info!("{} position is {}", id, position);
-        }
-        Ok(())
-    }
-
-    async fn move_base_to(&mut self, angle: f32, modifier: CommandModifier) -> Result<()> {
-        self.driver
-            .move_to_position_with_modifier(BASE_MOTOR_ID, angle, modifier)
-            .await?;
-        Ok(())
-    }
-
-    async fn move_neck_to(&mut self, angle: f32, speed: u32) -> Result<()> {
-        self.driver
-            .move_to_position_with_modifier(
-                NECK_MOTOR_A_ID,
-                angle,
-                CommandModifier::SpeedDegrees(speed),
-            )
-            .await?;
-        Ok(())
-    }
-
-    async fn wait_until_base_in_position(&mut self) -> Result<()> {
-        sleep(Duration::from_millis(500)).await;
-        loop {
-            let status = self.driver.query_status(BASE_MOTOR_ID).await?;
-            match status {
-                MotorStatus::Accelerating | MotorStatus::Decelerating | MotorStatus::Traveling => {}
-                _ => return Ok(()),
-            }
-            sleep(Duration::from_millis(250)).await;
-        }
-    }
-
-    async fn wait_until_neck_in_position(&mut self) -> Result<()> {
-        sleep(Duration::from_millis(500)).await;
-        loop {
-            let status = self.driver.query_status(NECK_MOTOR_A_ID).await?;
-            match status {
-                MotorStatus::Accelerating | MotorStatus::Decelerating | MotorStatus::Traveling => {}
-                _ => return Ok(()),
-            }
-            sleep(Duration::from_millis(250)).await;
-        }
-    }
-
-    async fn turn_off(&mut self) -> Result<()> {
-        for id in ALL_MOTOR_IDS {
-            self.driver.set_color(id, lss_driver::LedColor::Off).await?;
-        }
-        self.driver.limp(BASE_MOTOR_ID).await?;
-        Ok(())
-    }
-
-    async fn limp_neck(&mut self) -> Result<()> {
-        self.driver.limp(NECK_MOTOR_A_ID).await?;
-        Ok(())
-    }
 }
 
 #[tokio::main]
@@ -164,13 +73,4 @@ async fn main() -> anyhow::Result<()> {
     info!("finished");
 
     Ok(())
-}
-
-fn setup_tracing() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
+use std::time::Duration;
+
+use anyhow::Result;
+use lss_driver::{CommandModifier, LSSDriver, MotorStatus};
+use tokio::time::sleep;
 use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub mod configuration;
 pub mod error;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 pub async fn turn_on_display() -> anyhow::Result<()> {
-    // WAYLAND_DISPLAY="wayland-1" wlr-randr --output HDMI-A-1 --on --transform 90
+    // XDG_RUNTIME_DIR="/run/user/1000" WAYLAND_DISPLAY="wayland-1" wlr-randr --output HDMI-A-1 --on --transform 90
     let status = tokio::process::Command::new("wlr-randr")
+        .env("XDG_RUNTIME_DIR", "/run/user/1000")
         .env("WAYLAND_DISPLAY", "wayland-1")
         .arg("--output")
         .arg("HDMI-A-1")
@@ -23,8 +26,9 @@ pub async fn turn_on_display() -> anyhow::Result<()> {
 }
 
 pub async fn turn_off_display() -> anyhow::Result<()> {
-    // WAYLAND_DISPLAY="wayland-1" wlr-randr --output HDMI-A-1 --on --transform 90
+    // XDG_RUNTIME_DIR="/run/user/1000" WAYLAND_DISPLAY="wayland-1" wlr-randr --output HDMI-A-1 --off
     let status = tokio::process::Command::new("wlr-randr")
+        .env("XDG_RUNTIME_DIR", "/run/user/1000")
         .env("WAYLAND_DISPLAY", "wayland-1")
         .arg("--output")
         .arg("HDMI-A-1")
@@ -35,13 +39,109 @@ pub async fn turn_off_display() -> anyhow::Result<()> {
     Ok(())
 }
 
+const BASE_MOTOR_ID: u8 = 1;
+const NECK_MOTOR_A_ID: u8 = 2;
+
+const ALL_MOTOR_IDS: [u8; 2] = [BASE_MOTOR_ID, NECK_MOTOR_A_ID];
+
+pub const NECK_MOTOR_A_NORMAL_POSITION: f32 = -10.0;
+
+pub struct HeadController {
+    driver: LSSDriver,
+}
+
+impl HeadController {
+    pub fn new(port: &str) -> Result<Self> {
+        let driver = lss_driver::LSSDriver::new(port)?;
+        Ok(Self { driver })
+    }
+
+    pub async fn configure(&mut self) -> Result<()> {
+        for id in ALL_MOTOR_IDS {
+            self.driver.set_origin_offset(id, 0.0).await?;
+            self.driver
+                .configure_color(id, lss_driver::LedColor::Off)
+                .await?;
+            self.driver
+                .set_color(id, lss_driver::LedColor::Cyan)
+                .await?;
+
+            self.driver.set_motion_profile(id, true).await?;
+
+            let position = self.driver.query_position(id).await?;
+            info!("{} position is {}", id, position);
+        }
+        Ok(())
+    }
+
+    pub async fn move_base_to(&mut self, angle: f32, modifier: CommandModifier) -> Result<()> {
+        self.driver
+            .move_to_position_with_modifier(BASE_MOTOR_ID, angle, modifier)
+            .await?;
+        Ok(())
+    }
+
+    pub async fn move_neck_to(&mut self, angle: f32, speed: u32) -> Result<()> {
+        self.driver
+            .move_to_position_with_modifier(
+                NECK_MOTOR_A_ID,
+                angle,
+                CommandModifier::SpeedDegrees(speed),
+            )
+            .await?;
+        Ok(())
+    }
+
+    pub async fn wait_until_base_in_position(&mut self) -> Result<()> {
+        sleep(Duration::from_millis(500)).await;
+        loop {
+            let status = self.driver.query_status(BASE_MOTOR_ID).await?;
+            match status {
+                MotorStatus::Accelerating | MotorStatus::Decelerating | MotorStatus::Traveling => {}
+                _ => return Ok(()),
+            }
+            sleep(Duration::from_millis(250)).await;
+        }
+    }
+
+    pub async fn wait_until_neck_in_position(&mut self) -> Result<()> {
+        sleep(Duration::from_millis(500)).await;
+        loop {
+            let status = self.driver.query_status(NECK_MOTOR_A_ID).await?;
+            match status {
+                MotorStatus::Accelerating | MotorStatus::Decelerating | MotorStatus::Traveling => {}
+                _ => return Ok(()),
+            }
+            sleep(Duration::from_millis(250)).await;
+        }
+    }
+
+    pub async fn turn_off(&mut self) -> Result<()> {
+        for id in ALL_MOTOR_IDS {
+            self.driver.set_color(id, lss_driver::LedColor::Off).await?;
+        }
+        self.driver.limp(BASE_MOTOR_ID).await?;
+        Ok(())
+    }
+
+    pub async fn limp_neck(&mut self) -> Result<()> {
+        self.driver.limp(NECK_MOTOR_A_ID).await?;
+        Ok(())
+    }
+}
+
+pub fn setup_tracing() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
